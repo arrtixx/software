3bd08c36b6d27ac40015051b5c577407442ee57c
arrtixx <127791292+arrtixx@users.noreply.github.com>
2024-09-23 16:34:31 +0200

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-:: HTTP server running on port 19132
-@echo off
-title Arrtix_xSoftWare - HTTP server running on port 19132
-mode con: cols=191 lines=51
-echo [42m[97m[26;88HStarting server . . .[0m
-echo [46m[97m[27;84Hhttp://188.167.101.092:19132/[0m
-timeout 5 > nul
-cls
-python -m http.server 19132
